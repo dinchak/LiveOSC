04c5f2ae640b2d61dd6a6e87775a34acee8db366
tom <dinchak@gmail.com>
2016-03-27 15:38:05 -0700

    remove logging by default

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 import sys
 
-path = "/Users/mac/src/node-liveosc"
-errorLog = open(path + "/stderr.txt", "w")
-errorLog.write("Starting Error Log")
-sys.stderr = errorLog
-stdoutLog = open(path + "/stdout.txt", "w")
-stdoutLog.write("Starting Standard Out Log")
-sys.stdout = stdoutLog
+# path = "/Users/mac/src/node-liveosc"
+# errorLog = open(path + "/stderr.txt", "w")
+# errorLog.write("Starting Error Log")
+# sys.stderr = errorLog
+# stdoutLog = open(path + "/stdout.txt", "w")
+# stdoutLog.write("Starting Standard Out Log")
+# sys.stdout = stdoutLog
 
 from LiveOSC import LiveOSC
 
